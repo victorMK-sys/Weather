4d8cdba32bb579fef935b5fd4e923d92460421a4
VictorMK <victorkilonzi0@gmail.com>
2025-01-20 13:54:54 +0300

    Added weather elements

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const dateString = document.getElementById('currentDate')
 const date = new Date()
-const day = date.toLocaleDateString('en-US', {day: "numeric"})
-const month = date.toLocaleDateString('en-US', {month: "long"})
-const year = date.toLocaleDateString('en-US', {year: "numeric"})
+const day = date.toLocaleDateString('en-US', { day: "numeric" })
+const month = date.toLocaleDateString('en-US', { month: "long" })
+const year = date.toLocaleDateString('en-US', { year: "numeric" })
 
+dateString.innerHTML = `${day} ${month}, ${year}`
 
-function formatElements() {
-  
-  dateString.innerHTML = `${day} ${month}, ${year}`
-}
+window.addEventListener('load', () => {
+  const apiKey = 'YOUR_API_KEY';
+  const city = 'YOUR_CITY';
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-formatElements()
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      const weatherDescription = data.weather[0].description;
+      const temperature = data.main.temp;
+      const weatherElement = document.getElementById('weather');
+
+      weatherElement.innerHTML = `Weather: ${weatherDescription}, Temperature: ${temperature}°C`;
+    })
+    .catch(error => console.error('Error fetching weather data:', error));
+})
