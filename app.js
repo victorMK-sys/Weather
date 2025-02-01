de240fde678cf14061915d2fb10f2d83751a6962
victorMK-sys <victorkilonzi0@gmail.com>
2025-02-01 09:40:04 +0300

    Add files via upload
    Updated the getData() function and error handlers

--- app.js
+++ app.js
@@ -1,62 +1,84 @@
-const currentDate = document.getElementById('currentDate')
-const mainBody = document.querySelector('.weather-console')
-const currentWeather = document.getElementById('currentWeather')
-const cityName = document.getElementById('cityName')
-const weatherIcon = document.getElementById('weatherIcon')
+const weatherConsole = document.querySelector('.weather-console')
+const searchInput = document.getElementById('searchInput')
 
-async function getWeather(dateCallback, weatherCallback) {
-  const apiKey = '50dfacd827bba21242f91d2f77e2ef98'
-  const apiURL = `https://api.openweathermap.org/data/2.5/weather?units=metric&appid=${apiKey}&q=Kenya`
 
+function init() {
+  const API_KEY = '50dfacd827bba21242f91d2f77e2ef98'
+  let apiURL
+
+  if (searchInput.value !== '') {
+    let city = searchInput.value
+    searchInput.value = ''
+    apiURL = `https://api.openweathermap.org/data/2.5/weather?units=metric&appid=${API_KEY}&q=${city}`
+    getWeather(apiURL)
+  } else {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        let lat = position.coords.latitude
+        let lon = position.coords.longitude
+        apiURL = `https://api.openweathermap.org/data/2.5/weather?units=metric&appid=${API_KEY}&lat=${lat}&lon=${lon}`
+        getWeather(apiURL)
+      })
+    }
+  }
+}
+
+async function getWeather(apiURL) {
   try {
     const response = await fetch(apiURL)
-    if (!response.ok) {
-      offline()
-      throw new Error(`HTTP error! status: ${response.status}`)
-    }
-    const data = await response.json()
-    const timeStamp = data.dt * 1000
+    if (response.ok) {
+      const data = await response.json()
+      const timeStamp = data.dt * 1000
 
-    dateCallback(timeStamp)
-    weatherCallback(data)
+      formatDate(timeStamp)
+      formatWeather(data)
+      return
+    }
+    if (response.status == '401') throw new Error('401')
+    if (response.status == '404') throw new Error('404')
+
   } catch (error) {
-    console.error('Failed to fetch weather data:', error)
+    if (error.message == '401') offline('There might be a problem on our end. Please try again later')
+    if (error.message == '404') alert('The city/country entered does not exist')
+    return
   }
+
+  offline('You are offline. Please check your connection and try again')
 }
 
 function formatDate(timeStamp) {
   const date = new Date(timeStamp)
   const options = { year: 'numeric', month: 'long', day: 'numeric' }
 
-  currentDate.textContent = date.toLocaleString(undefined, options)
+  document.getElementById('currentDate').textContent = date.toLocaleString(undefined, options)
 }
 
 function formatWeather(data) {
-  const city = data.name
   const temperature = data.main.temp
 
-  currentWeather.textContent = `${temperature}°C`
-  cityName.textContent = city
+  document.getElementById('currentWeather').textContent = `${temperature}°C`
+  document.getElementById('cityName').textContent = data.name
 }
 
-function formatIcon(iconCode) {
-
+function formatIcon() {
 }
 
-function offline() {
-  let rmv = [...mainBody.children]
+function offline(message) {
+  let rmv = [...weatherConsole.children]
   rmv.forEach((el) => el.remove())
   const offlineImage = document.createElement('img')
   offlineImage.src = './images/main/offline-image.png'
   offlineImage.classList.add('offline-image')
   const offlineText = document.createElement('p')
-  offlineText.textContent = 'You are offline. Check your internet connection'
-  offlineText.classList.add('offline-text')
-
-  mainBody.appendChild(offlineImage)
-  mainBody.appendChild(offlineText)
+  offlineText.textContent = message
+  weatherConsole.appendChild(offlineImage)
+  weatherConsole.appendChild(offlineText)
 }
 
 window.addEventListener('load', () => {
-  getWeather(formatDate, formatWeather)
+  init()
+})
+
+document.querySelector('#searchButton').addEventListener('click', () => {
+  init()
 })
